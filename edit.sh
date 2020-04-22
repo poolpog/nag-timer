14e4cbf184ac161e8887faa9f58953ad502b1701
Pool Pog <poolpog@gmail.com>
2020-04-22 15:03:53 -0400

    Added manual editor

---
+++ edit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -x
+
+# Opens TRACKER_FILE in perferred editor for manual edits
+# TODO
+#   * use jq to validate JSON syntax after editing and deny edit of syntax failed
+
+cd $( dirname $0 )
+CWD=$( pwd -P )
+source "${CWD}/config.inc"
+
+if [[ -z "${EDITOR}" ]]; then
+    EDITOR="vim"
+fi
+
+${EDITOR} ${TRACKER_FILE}
