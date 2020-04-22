76c27786119eaf98ee70dff3ce184d2ee33d1f00
Pool Pog <poolpog@gmail.com>
2020-04-22 17:16:47 -0400

    make crontab works, maybe

--- make-crontab.sh
+++ make-crontab.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 
 cd $( dirname $0 )
 CWD=$( pwd -P )
@@ -11,6 +10,12 @@ SUB_PATH=$( echo "${CWD}/track.sh" | sed 's/\//\\\//g' )
 if [[ "${WHICH}"  ==  "LAUNCHCTL" ]]; then
     sed 's/PATH_TO_NAG_TIMER/'"${SUB_PATH}"'/' nag-launchctl-template.plist
 else
-    ( crontab -l; sed 's/PATH_TO_NAG_TIMER/'"${SUB_PATH}"'/' crontab.txt ) | sort | uniq
+    # This isn't perfect; it doesn't account for crontab variables very well
+    OLD="$( crontab -l )"
+    NEW="$( sed 's/PATH_TO_NAG_TIMER/'"${SUB_PATH}"'/' crontab.txt )"
+    CRON_VARS="$( printf "%s\n%s\n" "${OLD}" "${NEW}" | grep  '=' | sort | uniq )"
+    CRON_ENTRIES="$( printf "%s\n%s\n" "${OLD}" "${NEW}" | grep -v '=' | sort | uniq )"
+    echo "${CRON_VARS}"
+    echo "${CRON_ENTRIES}"
 fi
 
