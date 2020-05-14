321a6b264f4ba9fe240c3b5a2ae404e029ea702d
Pool Pog <poolpog@gmail.com>
2020-05-13 23:41:25 -0400

    updated nagtimer crontab path

--- bin/nag-make-crontab.sh
+++ bin/nag-make-crontab.sh
@@ -5,14 +5,14 @@ CWD=$( pwd -P )
 source "${CWD}/../lib/config.inc"
 
 WHICH="${1:-CRON}"
-SUB_PATH=$( echo "${CWD}/track.sh" | sed 's/\//\\\//g' )
+SUB_PATH=$( echo "${CWD}/nag-track.sh" | sed 's/\//\\\//g' )
 
 if [[ "${WHICH}"  ==  "LAUNCHCTL" ]]; then
     sed 's/PATH_TO_NAG_TIMER/'"${SUB_PATH}"'/' nag-launchctl-template.plist
 else
     # This isn't perfect; it doesn't account for crontab variables very well
     OLD="$( crontab -l )"
-    NEW="$( sed 's/PATH_TO_NAG_TIMER/ '"${SUB_PATH}"' /' crontab.txt )"
+    NEW="$( sed 's/PATH_TO_NAG_TIMER/ '"${SUB_PATH}"' /' ../lib/crontab.txt )"
     CRON_VARS="$( printf "%s\n%s\n" "${OLD}" "${NEW}" | grep  '=' | sort | uniq )"
     CRON_ENTRIES="$( printf "%s\n%s\n" "${OLD}" "${NEW}" | grep -v '=' | sort | uniq )"
     echo "${CRON_VARS}"
